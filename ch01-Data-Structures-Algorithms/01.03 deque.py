e82cac5c67217f85c8a019ecbcf413da743425ef
georstef <georstef@gmail.com>
2014-07-07 20:13:40 +0300

    01.03 deque

---
+++ ch01-Data-Structures-Algorithms/01.03 deque.py
@@ -0,0 +1,23 @@
+from collections import deque
+q = deque(maxlen=3)
+q.append(1)
+q.append(2)
+q.append(3)
+print(q)
+q.append(4)
+q.append(5)
+print(q)
+
+# -----------------------------
+
+q = deque()
+q.append(1)
+q.append(2)
+q.append(3)
+print(q)
+q.appendleft(4)
+print(q)
+q.append(5)
+print(q)
+q.popleft()
+print(q)
