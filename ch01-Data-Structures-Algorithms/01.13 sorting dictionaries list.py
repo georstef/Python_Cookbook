d67b2690a05aa9b96655535b8e8a51194e57708b
georstef <georstef@gmail.com>
2014-07-20 17:24:05 +0300

    01.13 sorting dictionaries list

---
+++ ch01-Data-Structures-Algorithms/01.13 sorting dictionaries list.py
@@ -0,0 +1,27 @@
+rows = [
+    {'fname': 'Brian', 'lname': 'Jones', 'uid': 1003},
+    {'fname': 'David', 'lname': 'Beazley', 'uid': 1002},
+    {'fname': 'John', 'lname': 'Cleese', 'uid': 1001},
+    {'fname': 'Big', 'lname': 'Jones', 'uid': 1004}
+    ]
+
+from operator import itemgetter
+
+import pprint
+pp = pprint.PrettyPrinter(indent=2)
+
+# sort by first name
+rows_by_fname = sorted(rows, key=itemgetter('fname'))
+# print(rows_by_fname)
+pp.pprint(rows_by_fname)
+print('')
+
+# sort by id
+rows_by_uid = sorted(rows, key=itemgetter('uid'))
+pp.pprint(rows_by_uid)
+print('')
+
+# sort first by last name and then with first name
+rows_by_lfname = sorted(rows, key=itemgetter('lname','fname')) # this is faster
+# it's the same like -> sorted(rows, key=lambda r: (r['lname'], r['fname']))
+pp.pprint(rows_by_lfname)
